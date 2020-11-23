9160cf973358ecef2ab9b94ac65f3142d4d95f8e
Muhammad <azhar40@live.co.uk>
2020-11-23 23:25:08 +0500

    Add project files.

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 52);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 282);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(360, 52);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(411, 282);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(360, 341);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(411, 282);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(16, 341);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(336, 282);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 641);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 699);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 699);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 764);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(316, 764);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 33);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(401, 699);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 33);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(401, 764);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 33);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(181, 275);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 107);
+            this.panel1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 46);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(812, 805);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        Image img;
+        string text;
+        int Tno = 0;
+        private int _tickets;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _tickets = 10;
+            images();
+            panel1.Hide();
+
+           
+        }
+       
+      
+        public void images()
+        {
+            textBox1.Text = "";
+            Tno = 5;
+            text = "QUAID";
+            textBox1.Text = "QUAI";
+            textBox1.Enabled = false;
+            img = Image.FromFile("places/1.jpg");
+            this.pictureBox1.Image = img;
+
+            img = Image.FromFile("places/3.jpg");
+            this.pictureBox2.Image = img;
+           
+            img = Image.FromFile("places/4.jpg");
+            this.pictureBox3.Image = img;
+           
+            img = Image.FromFile("places/2.jpg");
+            this.pictureBox4.Image = img;
+            
+            button1.Text = "D";
+            button2.Text = "E";
+            button3.Text = "X";
+            button4.Text = "T";
+            button5.Text = "O";
+            button6.Text = "N";
+            enable();
+        }
+        public void images1()
+        {
+          
+            Tno = 6;
+            textBox1.Text = "";
+            text = "ALTO";
+            textBox1.Text = "ALT";
+
+            img = Image.FromFile("cars/1.jpg");
+            this.pictureBox1.Image = img;
+
+            img = Image.FromFile("cars/2.jpg");
+            this.pictureBox2.Image = img;
+
+            img = Image.FromFile("cars/3.jpg");
+            this.pictureBox3.Image = img;
+
+            img = Image.FromFile("cars/4.jpg");
+            this.pictureBox4.Image = img;
+
+            button1.Text = "L";
+            button2.Text = "R";
+            button3.Text = "N";
+            button4.Text = "O";
+            button5.Text = "E";
+            button6.Text = "N";
+
+            enable();
+
+        }
+        public void images2()
+        {
+            Tno = 6;
+            textBox1.Text = "";
+            text = "TIGER";
+            textBox1.Text = "TIGE";
+
+            img = Image.FromFile("animal/1.jpg");
+            this.pictureBox1.Image = img;
+
+            img = Image.FromFile("animal/2.jpg");
+            this.pictureBox2.Image = img;
+
+            img = Image.FromFile("animal/3.jpg");
+            this.pictureBox3.Image = img;
+
+            img = Image.FromFile("animal/4.jpg");
+            this.pictureBox4.Image = img;
+
+            button1.Text = "D";
+            button2.Text = "O";
+            button3.Text = "T";
+            button4.Text = "V";
+            button5.Text = "E";
+            button6.Text = "R";
+            enable();
+
+
+        }
+        public void images3()
+        {
+            Tno = 6;
+            textBox1.Text = "";
+            text = "RAZER";
+            textBox1.Text = "RAZE";
+
+            img = Image.FromFile("mouse/1.jpg");
+            this.pictureBox1.Image = img;
+
+            img = Image.FromFile("mouse/2.jpg");
+            this.pictureBox2.Image = img;
+
+            img = Image.FromFile("mouse/3.jpg");
+            this.pictureBox3.Image = img;
+
+            img = Image.FromFile("mouse/4.png");
+            this.pictureBox4.Image = img;
+
+            button1.Text = "H";
+            button2.Text = "R";
+            button3.Text = "S";
+            button4.Text = "V";
+            button5.Text = "F";
+            button6.Text = "G";
+            enable();
+
+
+        }
+        public void images4()
+        {
+            Tno = 6;
+            textBox1.Text = "";
+            text = "APPLE";
+            textBox1.Text = "APPL";
+
+            img = Image.FromFile("mobile/1.png");
+            this.pictureBox1.Image = img;
+
+            img = Image.FromFile("mobile/2.jpg");
+            this.pictureBox2.Image = img;
+
+            img = Image.FromFile("mobile/3.png");
+            this.pictureBox3.Image = img;
+
+            img = Image.FromFile("mobile/4.jpg");
+            this.pictureBox4.Image = img;
+
+            button1.Text = "E";
+            button2.Text = "G";
+            button3.Text = "A";
+            button4.Text = "N";
+            button5.Text = "I";
+            button6.Text = "D";
+            enable();
+
+
+        }
+
+        int va = 0;
+      
+        public void check()
+        {
+            va++;
+            timer1.Stop();
+            if (textBox1.Text == text)
+            {
+
+                MessageBox.Show("Correct Answer");
+                score++;     
+
+            }
+           else if (textBox1.Text.Length == Tno || textBox1.Text != text)
+            {
+                MessageBox.Show("wrong");
+            }
+            if (va == 1)
+            {
+                images1();
+            }
+            else if (va == 2)
+            {
+                images2();
+            }
+            else if (va == 3)
+            {
+                images3();
+            }
+            else if (va == 4)
+            {
+                images4();
+
+            }
+            else
+            {
+
+                panel1.Show();
+                pictureBox1.Hide();
+                pictureBox2.Hide();
+                pictureBox3.Hide();
+                pictureBox4.Hide();
+                label2.Text = "your score is = " + score;
+                label1.Hide();
+                textBox1.Hide();
+                button1.Hide();
+                button2.Hide();
+                button3.Hide();
+                button4.Hide();
+                button5.Hide();
+                button6.Hide();
+            }
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = textBox1.Text + button1.Text;
+            button1.Enabled = false;
+            check();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text+ button2.Text;
+            button2.Enabled = false;
+            check();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button3.Text;
+            button3.Enabled = false;
+            check();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button4.Text;
+            button4.Enabled = false;
+            check();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button5.Text;
+            button5.Enabled = false;
+            check();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button6.Text;
+            button6.Enabled = false;
+            check();
+        }
+        public void enable()
+        {
+            _tickets = 60;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            _tickets--;
+            this.Text = _tickets.ToString();
+            label1.Text = this.Text;
+            if (_tickets == 0)
+            {
+
+                timer1.Stop();
+                MessageBox.Show("You Time is UP");
+                Application.Exit();
+            }
+
+        }
+      
+    }
+}
